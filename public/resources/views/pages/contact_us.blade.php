43587b82b10e9bfa131234c4f1ddba097069d92b
avitaind <52917489+avitaind@users.noreply.github.com>
2019-11-01 12:38:06 +0530

    Update contact_us.blade.php

--- public/resources/views/pages/contact_us.blade.php
+++ public/resources/views/pages/contact_us.blade.php
@@ -29,8 +29,8 @@
 
                             <div class="mb-2"><b>Address :</b><br>
                                 211B - 211C,  2nd Floor,  Elegance Tower, Jasola Vihar,  New Delhi-110025</div>
-                            <h2 class="contact_us_title">For Service :</h2>
-                            <div class="mb-2">+91 120 4848166</div>
+                            <h2 class="contact_us_title">For Service :</h2><span>+91 120 4848166</span>
+                            
 
                         </div>
                         <div class="col-sm-6">
