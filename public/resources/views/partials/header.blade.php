6d98fabd018fdd925b7c046e7b7d51d28973ac91
avitaind <52917489+avitaind@users.noreply.github.com>
2019-11-04 15:24:08 +0530

    Update header.blade.php

--- public/resources/views/partials/header.blade.php
+++ public/resources/views/partials/header.blade.php
@@ -159,7 +159,7 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/blog">Blog</a>
                 </li>  <li class="nav-item">
-                    <a class="nav-link" href="/diwali-campaign">Diwali Offer</a>
+                    <a class="nav-link" href="/diwali-campaign">Autumn Carnival Offer</a>
                 </li>
 
             </ul>
